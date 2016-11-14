75b4dcafdbec9414311490ab6e48b5e798bfb8b4
Thomas N Rizzo III <thomasnrizzo@gmail.com>
2016-11-13 21:37:07 -0700

    Configured knex files, installed dotenv

--- knexfile.js
+++ knexfile.js
@@ -3,41 +3,25 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: 'pg',
+    connection: {'postgres://localhost/myblog',
     migrations: {
-      tableName: 'knex_migrations'
+      directory:'./db/migrations',
+    },
+    seeds: {
+      directory:'./db/seeds'
     }
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
+    client: 'pg',
+    connection: {process.env.DATABASE_URL
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './db.migrations',
+    },
+    seeds: {
+      directory: './db/seeds'
     }
   }
 
