da0d793b2baf16be40ee7a5f0521139f8eadd190
Thomas N Rizzo III <thomasnrizzo@gmail.com>
2016-11-14 19:39:14 -0700

    Refactored migration to allow for additional characters in 'content' field.

--- db/migrations/20161113223359_newEntries.js
+++ db/migrations/20161113223359_newEntries.js
@@ -2,9 +2,9 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('newEntries', function(table){
     table.increments();
-    table.string('title');
-    table.string('content');
-    table.string('date');
+    table.string('title', [100]);
+    table.string('content', [1000]);
+    table.string('date', [10]);
   })
 };
 
